41f58c108f99f0f9bf510e70edf379d57f69d1d3
David <david.salazar@mercadolibre.com>
2022-09-07 12:49:21 -0300

    pass

--- app/src/main/java/com/example/notbored/SuggestionActivity.kt
+++ app/src/main/java/com/example/notbored/SuggestionActivity.kt
@@ -2,10 +2,33 @@ package com.example.notbored
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notbored.APIServices.APIService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SuggestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suggestion)
     }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("http://www.boredapi.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchRandom(query: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getActivities(query)
+            val activities = call.body()
+        }
+    }
 }
+
+
+
