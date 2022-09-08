cc11728cd8c84b63a147b25a305d564596f57af7
tbonillad <tatiana.bonilla@mercadolibre.com.co>
2022-09-08 13:13:40 -0500

    Creacion de servicios por tipo y participantes + tipo

--- app/src/main/java/com/example/notbored/APIServices/APIService.kt
+++ app/src/main/java/com/example/notbored/APIServices/APIService.kt
@@ -2,6 +2,7 @@ package com.example.notbored.APIServices
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface APIService {
@@ -11,6 +12,17 @@ interface APIService {
 
     @GET("activity/") //get request for an activity of random type
     suspend fun getRandomActivity(): Response<ActivityResponse>
+
+    @GET("activity/")
+    suspend fun getActivityByParticipantsAndType(
+        @Query("participants") participants:Int,
+        @Query("type") activity: String
+    ): Response<ActivityResponse>
+
+    @GET("activity/")
+    suspend fun getActivityByParticipants(
+        @Query("participants") participants:Int
+    ): Response<ActivityResponse>
 
 
     //TODO get request with type and participants
