33eb9ee47db4344e48751d5c9b1fc68e7403640e
tbonillad <tatiana.bonilla@mercadolibre.com.co>
2022-09-08 11:52:30 -0500

    se crean los metodos validateFieldParticipants, validateEnableButton, oClick y isNumeric

--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.notbored
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnStart = findViewById<Button>(R.id.btnStart)
+        var edtParticipants = findViewById<EditText>(R.id.editTextNumber)
+
+        validateEnableButton(btnStart, edtParticipants)
+        onClick(edtParticipants, btnStart)
+
+    }
+
+    private fun onClick(edtParticipants: EditText, btnStart: Button) {
+        btnStart.setOnClickListener {
+            var intent = Intent(this, CategoriesActivity::class.java)
+            intent.putExtra(
+                "participants",
+                edtParticipants.toString()
+            ) //esta enviando el dato pero en string :(
+            startActivity(intent)
+        }
+    }
+
+    private fun validateEnableButton(btnStart: Button, edtParticipants: EditText) {
+        btnStart.isEnabled = false
+
+        edtParticipants.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                when {
+                    !validateFieldParticipants(edtParticipants) -> btnStart.isEnabled = false
+                    else -> btnStart.isEnabled = true
+                }
+            }
+        })
+    }
+
+    private fun validateFieldParticipants(edtParticipants: EditText): Boolean {
+        val numberOfParticipants = edtParticipants.text.toString()
+        return when {
+            numberOfParticipants.isNullOrEmpty()
+                    || !isNumeric(numberOfParticipants)
+                    || numberOfParticipants.toInt() <= 0 -> false
+            else -> true
+        }
+    }
+
+    private fun isNumeric(toCheck: String): Boolean {
+        return toCheck.all { char -> char.isDigit() }
     }
 }
